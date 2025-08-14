d9e57964bfe74ed88d19768f9d840fd955f1b1d7
Andrei Brizhinev <bragovo@gmail.com>
2025-08-14 17:11:39 +0300

    Bump prost & tonic

--- lib/api/build.rs
+++ lib/api/build.rs
@@ -3,12 +3,12 @@ use std::{env, path::PathBuf};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("auth_descriptor.bin"))
         .compile_protos(&["src/auth.proto"], &["src"])
         .unwrap();
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("users_descriptor.bin"))
         .compile_protos(&["src/users.proto"], &["src"])
         .unwrap();
